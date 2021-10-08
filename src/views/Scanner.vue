2fb505a874398ad01554b2433dbf73cadf762eb3
grzegorzjeremenko <grzesiula002@gmail.com>
2021-10-08 03:58:32 +0200

    Fixed scanner

--- src/views/Scanner.vue
+++ src/views/Scanner.vue
@@ -1,6 +1,8 @@
 <template>
-  <div
-    class="scanner">
+  <div class="scanner">
+    <i 
+      class="icon-left-open"
+      v-on:click="navigateTo('/news')"></i>
     <ScannerComp />
   </div>
 </template>
@@ -12,10 +14,23 @@ export default {
   name: 'Scanner',
   components: {
       ScannerComp
+  },
+  methods: {
+    navigateTo: function(subpage) {
+      if(this.$route.path != subpage) 
+        this.$router.push(subpage)
+    }
   }
 }
 </script>
 
 <style scoped>
-
+  div.scanner i {
+    position: fixed;
+    top: 30px;
+    left: 30px;
+    font-size: 30px;
+    color: #fff;
+    z-index: 999;
+  }
 </style>
